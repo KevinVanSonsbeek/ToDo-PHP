6f8cca4a5f246a08398130cbafcec52690d47f35
Kevin van Sonsbeek <99041317@mydavinci.nl>
2018-03-10 14:55:30 +0100

    Update database.sql
    Switched back to the heidi.sql file

--- database.sql
+++ database.sql
@@ -1,99 +1,54 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.9
--- https://www.phpmyadmin.net
---
--- Host: localhost
--- Generation Time: Mar 10, 2018 at 01:50 PM
--- Server version: 5.6.37
--- PHP Version: 5.6.31
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
-
+-- --------------------------------------------------------
+-- Host:                         127.0.0.1
+-- Server versie:                5.6.37 - MySQL Community Server (GPL)
+-- Server OS:                    Win32
+-- HeidiSQL Versie:              9.5.0.5196
+-- --------------------------------------------------------
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!40101 SET NAMES utf8 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 
---
--- Database: `php_todo`
---
 
--- --------------------------------------------------------
+-- Databasestructuur van php_todo wordt geschreven
+CREATE DATABASE IF NOT EXISTS `php_todo` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `php_todo`;
 
---
--- Table structure for table `lists`
---
-
+-- Structuur van  tabel php_todo.lists wordt geschreven
 CREATE TABLE IF NOT EXISTS `lists` (
-  `list_id` int(11) NOT NULL,
+  `list_id` int(11) NOT NULL AUTO_INCREMENT,
   `list_name` varchar(255) NOT NULL,
-  `list_description` varchar(255) NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+  `list_description` varchar(255) NOT NULL,
+  PRIMARY KEY (`list_id`),
+  KEY `list_id` (`list_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `lists`
---
-
+-- Dumpen data van tabel php_todo.lists: ~1 rows (ongeveer)
+/*!40000 ALTER TABLE `lists` DISABLE KEYS */;
 INSERT INTO `lists` (`list_id`, `list_name`, `list_description`) VALUES
-(4, 'Example List', 'This list is an example list');
+	(1, 'List Number unos', 'Small list description');
+/*!40000 ALTER TABLE `lists` ENABLE KEYS */;
 
--- --------------------------------------------------------
-
---
--- Table structure for table `tasks`
---
-
+-- Structuur van  tabel php_todo.tasks wordt geschreven
 CREATE TABLE IF NOT EXISTS `tasks` (
-  `task_id` int(11) NOT NULL,
+  `task_id` int(11) NOT NULL AUTO_INCREMENT,
   `list_id` int(11) NOT NULL,
   `task_name` varchar(50) NOT NULL,
   `task_description` varchar(50) NOT NULL,
   `task_status` enum('Completed','In Progress','Concept') NOT NULL,
-  `task_duration` time NOT NULL
-) ENGINE=InnoDB AUTO_INCREMENT=21 DEFAULT CHARSET=latin1;
+  `task_duration` time NOT NULL,
+  UNIQUE KEY `task_id` (`task_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=latin1;
 
---
--- Dumping data for table `tasks`
---
-
+-- Dumpen data van tabel php_todo.tasks: ~2 rows (ongeveer)
+/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
 INSERT INTO `tasks` (`task_id`, `list_id`, `task_name`, `task_description`, `task_status`, `task_duration`) VALUES
-(18, 4, 'Example task', 'bla bla bla', 'Concept', '03:00:00'),
-(19, 4, 'Example task 2', 'Task 2', 'In Progress', '10:00:00'),
-(20, 4, 'Test', 'asdfasdf', 'Completed', '03:00:00');
+	(10, 1, '1', '123123', 'Concept', '00:00:00'),
+	(11, 1, '3', '123123', 'Completed', '00:00:00');
+/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
 
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `lists`
---
-ALTER TABLE `lists`
-  ADD PRIMARY KEY (`list_id`),
-  ADD KEY `list_id` (`list_id`);
-
---
--- Indexes for table `tasks`
---
-ALTER TABLE `tasks`
-  ADD UNIQUE KEY `task_id` (`task_id`);
-
---
--- AUTO_INCREMENT for dumped tables
---
-
---
--- AUTO_INCREMENT for table `lists`
---
-ALTER TABLE `lists`
-  MODIFY `list_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `tasks`
---
-ALTER TABLE `tasks`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=21;
+/*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
+/*!40014 SET FOREIGN_KEY_CHECKS=IF(@OLD_FOREIGN_KEY_CHECKS IS NULL, 1, @OLD_FOREIGN_KEY_CHECKS) */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
